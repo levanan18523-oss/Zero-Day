59c8650f9d0955ff3288cad16ecb9a2393e33f31
levanan18523-oss <levanan18523@gmail.com>
2026-06-09 07:38:21 +0700

    Update index.test.js
    Kiểm thử ung dụng

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,1 +1,11 @@
+// Dùng để đảm bảo code chạy đúng như mong đợi.
 
+const { sayHello, add } = require('../src/index');
+
+test('sayHello trả về đúng chuỗi chào', () => {
+    expect(sayHello('Phát')).toBe('Xin chào, Phát!');
+});
+
+test('add tính đúng tổng 2 số', () => {
+    expect(add(2, 3)).toBe(5);
+});
