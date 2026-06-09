3e442ba3ee86588b9568e0b0c8d1e7ec0eb9da42
levanan18523-oss <levanan18523@gmail.com>
2026-06-09 07:27:29 +0700

    Update index.js
    Mã nguồn chính của dự án

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+//Giải thích: Đây là file chính của ứng dụng.
+//Nơi bạn viết code xử lý logic chính.
 
+function sayHello(name) {
+    return `Xin chào, ${name}!`;
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+// Xuất hàm để có thể kiểm thử (test)
+module.exports = { sayHello, add };
